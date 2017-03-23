b5243fb09edccefd85ffc91088829e3a6027eba4
Margatroidz <as80110680@gmail.com>
2017-03-23 08:35:08 +0800

    no message

--- Framework/Audio.cpp
+++ Framework/Audio.cpp
@@ -2,8 +2,16 @@
 #include "XAudio2.h"
 
 struct Audio::Impl {
+	Impl();
+	~Impl();
 	IXAudio2SourceVoice* _sourceVoice;
 };
+
+Audio::Impl::Impl() {
+	_sourceVoice = nullptr;
+};
+
+Audio::Impl::~Impl() {};
 
 Audio::Audio(char* sourcePath) :pimpl(new Impl) {
 	pimpl->_sourceVoice = XAudio2::Instance()->LoadWave(sourcePath);
